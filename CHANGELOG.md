16dafda12017ca4a6bc23963969c20d326f35891
Kevin Saucedo <developer2@sfperusac.com>
2023-11-30 12:45:51 -0500

    fix context box, paddings and more

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,25 +1,32 @@
 ## 0.0.1
 
 - Initial release.
-    - showKDialogWithLoadingMessage
-    - showKDialogWithLoadingIndicator
-    - showBottomAlertKDialog
-    - showKDialogContent
-    - showConfirmationKDialog
-    - showAsyncProgressKDialog
+  - showKDialogWithLoadingMessage
+  - showKDialogWithLoadingIndicator
+  - showBottomAlertKDialog
+  - showKDialogContent
+  - showConfirmationKDialog
+  - showAsyncProgressKDialog
 
 ## 0.0.2
 
 - Fix showConfirmationKDialog and showBottomAlertKDialog response
-    - response false by default
-- Fix showConfirmationKDialog width when message is short 
+  - response false by default
+- Fix showConfirmationKDialog width when message is short
 
 ## 0.0.3
-- Refactor 
+
+- Refactor
 - Add options dialogs
 
 ## 0.1.0
+
 - Improvements in the way objects are compared in the options dialog
 
 ## 0.1.1
+
 - Fix result on options dialogs when cancel button is pressed, returns null now
+
+## 1.0.0
+
+- Stable version
--- lib/src/show_kdialog_content.dart
+++ lib/src/show_kdialog_content.dart
@@ -4,72 +4,86 @@ Future<T?> showKDialogContent<T>(
   BuildContext context, {
   EdgeInsetsGeometry contentPadding = const EdgeInsets.symmetric(horizontal: 24),
   EdgeInsetsGeometry scrollPadding = const EdgeInsets.only(bottom: 24),
+  EdgeInsetsGeometry titlePading = const EdgeInsets.only(right: 8.0, left: 8.0, top: 5.0),
   TextButton? titleTextButton,
   String? title = "Title!",
   String saveBtnText = "Save",
   void Function()? onSave,
   bool closeOnOutsideTab = false,
+  bool batToClose = true,
+  bool hideTitileBar = false,
+  bool allowBackButtonToClose = true,
+  Color? backgroundColor,
   required Widget Function(BuildContext context) builder,
 }) async {
+  Widget? titleWidget;
+  if (!hideTitileBar) {
+    titleWidget = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          iconSize: 25,
+          onPressed: () => Navigator.of(context).pop(null),
+          icon: const Icon(Icons.close),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 1.0),
+          child: Text(
+            title ?? "",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Visibility(
+                visible: onSave != null,
+                child: TextButton(
+                  onPressed: () => {if (onSave != null) onSave()},
+                  style: Theme.of(context).textButtonTheme.style,
+                  child: Text(saveBtnText),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
   return await showDialog(
     context: context,
     barrierDismissible: closeOnOutsideTab,
     useSafeArea: true,
     builder: (context) {
-      return AlertDialog(
-        titlePadding: const EdgeInsets.only(right: 8.0, left: 8.0, top: 5.0),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(
-              iconSize: 25,
-              onPressed: () => Navigator.of(context).pop(null),
-              icon: const Icon(Icons.close),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 1.0),
-              child: Text(
-                title ?? "",
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
+      return WillPopScope(
+        onWillPop: () async => allowBackButtonToClose,
+        child: AlertDialog(
+          backgroundColor: backgroundColor,
+          titlePadding: titlePading,
+          title: titleWidget,
+          contentPadding: EdgeInsets.zero,
+          content: SingleChildScrollView(
+            padding: scrollPadding,
+            child: Padding(
+              padding: contentPadding,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Visibility(
-                    visible: onSave != null,
-                    child: TextButton(
-                      onPressed: () => {if (onSave != null) onSave()},
-                      style: Theme.of(context).textButtonTheme.style,
-                      child: Text(saveBtnText),
-                    ),
-                  ),
+                  const SizedBox(width: double.maxFinite),
+                  builder(context),
                 ],
               ),
-            )
-          ],
-        ),
-        contentPadding: EdgeInsets.zero,
-        content: SingleChildScrollView(
-          padding: scrollPadding,
-          child: Padding(
-            padding: contentPadding,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(width: double.maxFinite),
-                builder(context),
-              ],
             ),
           ),
+          actionsPadding: EdgeInsets.zero,
         ),
       );
     },
